1ff8426fbe2d28b3d5fb85dd3e13d554faaa1805
leontev1 <dmitryleontev@inbox.ru>
2023-04-24 14:53:12 +0300

    Initial commit

---
+++ src/main/java/com/leontev/MyWebsiteApi/MyWebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.leontev.MyWebsiteApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebsiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebsiteApiApplication.class, args);
+	}
+
+}
